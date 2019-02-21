c1160a1a56521d786b86b8af1976143dcafd108d
porosb <porosenie@gmail.com>
2019-02-21 11:09:08 +0300

    Ticket has_one Excavator

--- app/models/excavator.rb
+++ app/models/excavator.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class Excavator < ApplicationRecord
+  belongs_to :ticket
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Ticket < ApplicationRecord
+  has_one :excavator
+
+  accepts_nested_attributes_for :excavator
 end
