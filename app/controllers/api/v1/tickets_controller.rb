7dd9fc8075cefa482904de46a07a67a0622f09df
porosb <porosenie@gmail.com>
2019-02-21 03:28:35 +0300

    add TicketsController [:create] and Api:V1:TicketsController [:show, :index]

---
+++ app/controllers/api/v1/tickets_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::TicketsController < ApplicationController
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :tickets, only: :create
+    end
+  end
+
+  resources :tickets, only: %i[index show]
 end
